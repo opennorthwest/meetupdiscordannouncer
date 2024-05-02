6a110f9479d7ba9d669ab1555fbb767ae97a6713
David Gibbons <david.gibbons@vevo.com>
2024-05-02 11:11:48 -0700

    improve linting, fix whitespace

--- meetupRunner.py
+++ meetupRunner.py
@@ -9,43 +9,45 @@ from discord_webhook import DiscordWebhook
 
 
 def get_events(group):
-  # api-endpoint
-  URL = "https://api.meetup.com/%s/events?&sign=true&photo-host=public&page=10" % (group)
-  # sending get request and saving the response as response object
-  r = requests.get(url=URL)
-  return r.json()
+    # api-endpoint
+    url = "https://api.meetup.com/%s/events?&sign=true&photo-host=public&page=10" % (group)
+    # sending get request and saving the response as response object
+    res = requests.get(url=url)
+    return res.json()
 
 
 def build_weekly_msg(event, event_date):
-  message = 'Don\'t forget to sign up for {name} on {weekday}\nRSVP here: <{link}>'.format(name=event['name'], link=event['link'], weekday=calendar.day_name[event_date.weekday()])
-  return message
+    message = 'Don\'t forget to sign up for {name} on {weekday}\nRSVP here: <{link}>'.format(name=event['name'], link=event['link'], weekday=calendar.day_name[event_date.weekday()])
+    return message
 
 
 def build_message(event):
-  message = "Join us today at %s for %s.\n %s people attending so far! Sign up: <%s>" % (event['local_time'], event['name'], event['yes_rsvp_count'], event['link'])
-  return message
+    message = "Join us today at %s for %s.\n %s people attending so far! Sign up: <%s>" % (event['local_time'], event['name'], event['yes_rsvp_count'], event['link'])
+    return message
 
 
 def publish_message(discord_url, msg):
-  webhook = DiscordWebhook(url=discord_url, content=message)
-  response = webhook.execute()
+    webhook = DiscordWebhook(url=discord_url, content=message)
+    response = webhook.execute()
 
+def is_sunday():
+    return now.weekday() == 6
 
 discord_url = os.environ.get('DISCORD_URL')
 group = os.environ.get('MEETUP_GROUP')
 if not discord_url and group:
-  print('Missing a required environment variable')
-  sys.exit(1)
+    print('Missing a required environment variable')
+    sys.exit(1)
 events = get_events(group)
 
 now = datetime.datetime.today()
-sunday = True if now.weekday() == 6 else False
+
 messages = []
 for event in events:
-  event_date = datetime.datetime(*(time.strptime(event['local_date'], '%Y-%m-%d')[0:6]))
-  if event_date.date() <= now.date() + datetime.timedelta(days=7 if sunday else 1):
-    messages.append(build_weekly_msg(event, event_date) if sunday else build_message(event))
+    event_date = datetime.datetime(*(time.strptime(event['local_date'], '%Y-%m-%d')[0:6]))
+    if event_date.date() <= now.date() + datetime.timedelta(days=7 if is_sunday() else 1):
+        messages.append(build_weekly_msg(event, event_date) if is_sunday() else build_message(event))
 
 for message in messages:
-  print(message)
-  publish_message(discord_url, message)
+    print(message)
+    publish_message(discord_url, message)
